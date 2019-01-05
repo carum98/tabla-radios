02798efc174dc68de439a12da3ca9033f4da6cf7
Carlos <carumtito@gmail.com>
2019-01-05 12:12:34 -0600

    Formulario para agregar radio completo

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 new Vue({
     el: '#app',
     data:{
+        radios: [
+            {}
+        ],
+
+        nombre:'',
+        imei: '',
+        sim:'',
         modelo: '',
-        nombre: ''
+        cellphone: '',
+        cargador: '',
+        estado: ''
+        
+    },
+    methods:{
+        nuevoRadio: function(){
+
+            this.radios.push({
+                nombre:this.nombre ,imei: this.imei, sim: this.sim, modelo: this.modelo, cellphone: this.cellphone, estado: this.estado, cargador: this.cargador });
+                
+                this.nombre = "";
+                this.imei = "";
+                this.sim = "";
+                this.cellphone = "";
+        }
     }
 })
+
