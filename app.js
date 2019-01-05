6bc994b0ea5592a5b5f449a75cc2bb23ba115ba9
Carlos <carumtito@gmail.com>
2019-01-05 17:13:53 -0600

    Formularios y tablas completas para mostrar radios e iformacion

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 new Vue({
-    el: '#app',
+    el: '#radio',
     data:{
-        radios: [
-            {}
-        ],
+        radios: [],
 
         nombre:'',
         imei: '',
@@ -19,11 +17,43 @@ new Vue({
 
             this.radios.push({
                 nombre:this.nombre ,imei: this.imei, sim: this.sim, modelo: this.modelo, cellphone: this.cellphone, estado: this.estado, cargador: this.cargador });
-                
+    
+                var radios = this.radios
+                console.log(radios)
+
                 this.nombre = "";
                 this.imei = "";
                 this.sim = "";
                 this.cellphone = "";
+
+
+        }
+    }
+})
+
+new Vue({
+    el:'#datos',
+    data:{
+        datos: [ 
+        ]  
+        ,
+
+        cliente: '',
+        ejecutivo: '',
+        modalidad: '',
+        fecha: '',
+        mostrar: true,
+    },
+    methods:{
+        agregarDatos: function(){
+
+            this.datos.push({cliente: this.cliente, ejecutivo: this.ejecutivo, modalidad: this.modalidad, fecha: this.fecha})
+
+            this.mostrar = false;
+            this.cliente = "";
+            this.ejecutivo = "";
+            this.modalidad = "";
+            this.fecha = "";
         }
     }
 })
